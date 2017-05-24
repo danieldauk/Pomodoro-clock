dc3cc501857e3db84a3c71beaa28f93b85720ef2
danieldauk <danieldauk@gmail.com>
2017-05-24 19:33:55 +0300

    second draft

--- js/main.js
+++ js/main.js
@@ -1,33 +1,51 @@
 var breakValue = 5;
 var workValue = 20;
 
-$("#break").roundSlider({
-    sliderType: "default",
-    value: 5,
-    circleShape: "half-top",
-    max: 30,
-    radius: 80,
-    handleSize: "34,10",
-    change: "setBreak"
-});
-
-$("#work").roundSlider({
-    sliderType: "default",
-    value: 20,
-    circleShape: "half-top",
-    max: 60,
-    radius: 80,
-    handleSize: "34,10",
-    change: "setWork"
-});
-
-
-function setBreak () {
-  breakValue = $("#break").roundSlider("getValue");
+$(function() {
   
-}
+    var progressCircle = new ProgressBar.Circle('#clock', {
+      strokeWidth: 5,
+      fill: "rgba(0,0,0,0.1)",
+      text: {
+        value: "20:00"
+      }
+  });
+  progressCircle.animate(1, {
+      duration: 1000
+  });
+
+  $("#break").roundSlider({
+      sliderType: "min-range",
+      value: 5,
+      circleShape: "half-top",
+      min: 1,
+      max: 30,
+      radius: 80,
+      handleSize: "34,10",
+      change: "setBreak"
+  });
+
+  $("#work").roundSlider({
+      sliderType: "min-range",
+      value: 20,
+      circleShape: "half-top",
+      min: 1,
+      max: 60,
+      radius: 80,
+      handleSize: "34,10",
+      change: "setWork"
+  });
 
 
-function setWork() {
+  function setBreak () {
+    breakValue = $("#break").roundSlider("getValue");
+    
+
+  }
+
+
+  function setWork() {
+    workValue = $("#work").roundSlider("getValue");
+  }
   
-}
+})
